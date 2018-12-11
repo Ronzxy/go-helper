19d74d9fb87c80699dbd61d0d5d3fa413504084c
sky <skygangsta@hotmail.com>
2018-12-11 13:52:02 +0800

    Organize Format

--- date.go
+++ date.go
@@ -23,66 +23,40 @@ func NewDate() *Date {
 	return &Date{}
 }
 
+/**
+ * 使用易于理解的字符串格式化时间，Y-m-d H:M:S.ms
+ */
 func (this *Date) Format(t time.Time, format string) string {
-	//patterns := []string{
-	//	// year
-	//	"Y", "2006", // A full numeric representation of a year, 4 digits	Examples: 1999 or 2003
-	//	"y", "06", //A two digit representation of a year	Examples: 99 or 03
-	//
-	//	// month
-	//	"m", "01", // Numeric representation of a month, with leading zeros	01 through 12
-	//	"n", "1", // Numeric representation of a month, without leading zeros	1 through 12
-	//	"M", "Jan", // A short textual representation of a month, three letters	Jan through Dec
-	//	"F", "January", // A full textual representation of a month, such as January or March	January through December
-	//
-	//	// day
-	//	"d", "02", // Day of the month, 2 digits with leading zeros	01 to 31
-	//	"j", "2", // Day of the month without leading zeros	1 to 31
-	//
-	//	// week
-	//	"D", "Mon", // A textual representation of a day, three letters	Mon through Sun
-	//	"l", "Monday", // A full textual representation of the day of the week	Sunday through Saturday
-	//
-	//	// time
-	//	"g", "3", // 12-hour format of an hour without leading zeros	1 through 12
-	//	"G", "15", // 24-hour format of an hour without leading zeros	0 through 23
-	//	"h", "03", // 12-hour format of an hour with leading zeros	01 through 12
-	//	"H", "15", // 24-hour format of an hour with leading zeros	00 through 23
-	//
-	//	"a", "pm", // Lowercase Ante meridiem and Post meridiem	am or pm
-	//	"A", "PM", // Uppercase Ante meridiem and Post meridiem	AM or PM
-	//
-	//	"i", "04", // Minutes with leading zeros	00 to 59
-	//	"s", "05", // Seconds, with leading zeros	00 through 59
-	//
-	//	"S", "000", // millisecond
-	//}
 	patterns := []string{
 		// two letter
-		"ms", "000", 		// millisecond
-		"mi", "00000", 		// microseconds
-		"ns", "00000000", 	// nanoseconds
+		"ms", "000", // millisecond
+		"mi", "000000", // microseconds
+		"ns", "000000000", // nanoseconds
 
 		// year
-		"Y", "2006", 	// A full numeric representation of a year, 4 digits	Examples: 1999 or 2003
-		"y", "06", 		//A two digit representation of a year	Examples: 99 or 03
+		"Y", "2006", // A full numeric representation of a year, 4 digits	Examples: 1999 or 2003
+		"y", "06", // A two digit representation of a year	Examples: 99 or 03
 
 		// month
-		"m", "01", 		// Numeric representation of a month, with leading zeros	01 through 12
-		"n", "1", 		// Numeric representation of a month, without leading zeros	1 through 12
+		"m", "01", // Numeric representation of a month, with leading zeros	01 through 12
+		"n", "1", // Numeric representation of a month, without leading zeros	1 through 12
 
 		// day
-		"d", "02", 		// Day of the month, 2 digits with leading zeros	01 to 31
-		"j", "2", 		// Day of the month without leading zeros	1 to 31
+		"d", "02", // Day of the month, 2 digits with leading zeros	01 to 31
+		"e", "2", // Day of the month without leading zeros	1 to 31
 
 		// time
-		"H", "15", 		// 24-hour format of an hour with leading zeros	00 through 23
-		"M", "04", 		// Minutes with leading zeros	00 to 59
-		"S", "05", 		// Seconds, with leading zeros	00 through 59
+		"H", "15", // 24-hour format of an hour with leading zeros	00 through 23
+		"M", "04", // Minutes with leading zeros	00 to 59
+		"S", "05", // Seconds, with leading zeros	00 through 59
 	}
 
 	replacer := strings.NewReplacer(patterns...)
 	format = replacer.Replace(format)
 
 	return t.Format(format)
+}
+
+func (this *Date) DefaultFormat(t time.Time) string {
+	return this.Format(t, "Y-m-d H:M:S.ms")
 }
